97434e76de39e5dbc4454d7ec7aafa3e501422c9
caotic7 <gitausimonkaranja@gmail.com>
2026-03-21 02:31:02 +0300

    fix(lenis): add explicit type for easing parameter to satisfy TS build

--- src/components/LenisScrollProvider.tsx
+++ src/components/LenisScrollProvider.tsx
@@ -11,7 +11,7 @@ export default function LenisScrollProvider({
   useEffect(() => {
     const lenis = new Lenis({
       duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
       direction: "vertical", // vertical, horizontal
       gestureDirection: "vertical", // vertical, horizontal, both
       smooth: true,
